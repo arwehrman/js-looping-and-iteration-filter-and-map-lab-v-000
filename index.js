35e818ae49bca8dcd44684e91d5ec70b981cd3d9
Amanda Wehrman <arwehrman@gmail.com>
2018-01-17 01:06:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function driversWithRevenueOver(drivers, revenue){
   return drivers.filter(function(revenue){
-    console.log(function(revenue));
+    console.log(function(revenue))
     drivers.revenue > revenue
   })
 }
